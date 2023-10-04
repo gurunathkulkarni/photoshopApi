aca440668cbf9ed814937eadb002631b610db90b
Gurunath <gurunathkulkarni96@gmail.com>
2023-10-04 08:01:31 +0530

    post change

--- index.js
+++ index.js
@@ -277,7 +277,7 @@ app.get("/delete/happyCustomer/:id", async (req, res) => {
 
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
